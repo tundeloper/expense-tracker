b2ffa89459e95a14207cdd39d17d3be0d5d5b3d1
Babatunde <68726835+tundeloper@users.noreply.github.com>
2023-08-04 20:06:02 +0100

    setting app navigation

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import MangeExpenses from './screens/ManageExpenses';
+import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
+import { GlobaStyles } from './constants/styles';
+
+const Stack = createNativeStackNavigator()
+const BotomTabs = createBottomTabNavigator()
+
+function ExpenpesesOverview() {
+  console.log(GlobaStyles)
+  return (
+    <BotomTabs.Navigator screenOptions={{
+      headerStyle: {backgroundColor: 'red'}
+    }}>
+      <BotomTabs.Screen name='RecentExpenses' component={RecentExpenses} />
+      <BotomTabs.Screen name='AllExpenses' component={AllExpenses} />
+    </BotomTabs.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Heloo world !!!!!</Text>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='ExpensesOverview'>
+          <Stack.Screen name='ExpensesOverview' component={ExpenpesesOverview} options={{headerShown: false}}/> 
+          <Stack.Screen name='ManageExpense' component={MangeExpenses} />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/ManafeExpenses.js
+++
@@ -1,7 +0,0 @@
-import { View } from "react-native";
-
-export default function ManafeExpenses() {
-  return (
-    <Text>ManafeExpenses</Text>
-  )
-}
